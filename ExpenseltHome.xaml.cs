e6de5cfe7c18758a0e865e70e089b6fa5dee45d7
kymifBSM <sandmanfin@gmail.com>
2023-05-26 14:52:59 +0700

    complete

---
+++ ExpenseltHome.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_laba_15
+{
+    /// <summary>
+    /// Логика взаимодействия для ExpenseltHome.xaml
+    /// </summary>
+    public partial class ExpenseltHome : Page
+    {
+        public ExpenseltHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
